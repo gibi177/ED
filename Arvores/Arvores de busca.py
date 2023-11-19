6c5830ed8c3f734653528f2d7e217a37baa55510
Felipe Gibin <gibinf.duarte@gmail.com>
2023-11-19 19:09:43 -0300

    metodo delete e search arvores de busca

--- Arvores/Arvores de busca.py
+++ Arvores/Arvores de busca.py
@@ -9,30 +9,27 @@ class BinarySearchTree:
         self.root = Node(data)
 
     def insert(self, data):
-        new_node = Node(data)
+        def insert_recursive(data,current):
+            new_node= Node(data)
 
-        if self.root is None:
-            self.root = new_node
-        else:
-            current = self.root
+            if current==None:
+                self.root=new_node
 
-            while True:
-                if data < current.data:
-                    if current.left is None:
-                        current.left = new_node
-                        break
-                    else:
-                        current = current.left
-                elif data > current.data:
-                    if current.right is None:
-                        current.right = new_node
-                        break
-                    else:
-                        current = current.right
+            elif data<current.data:
+                if current.left==None:
+                    current.left=new_node
                 else:
-                    break
+                    insert_recursive(data,current.left)
+            
+            elif data>current.data:
+                if current.right==None:
+                    current.right=new_node
+                else:
+                    insert_recursive(data,current.right)
+              
+        insert_recursive(data,self.root)
     
-    def print(self):
+    def print(self): #preorder
         def print_rec(node, level, direction):
             if node:
                 print("-" * level, node.data, direction)
@@ -42,24 +39,72 @@ class BinarySearchTree:
         print_rec(self.root, 0, 'Root')
     
     def height(self):
-         def heightRec(node):
-             if node:
+        def heightRec(node):
+            if node:
                 hl = heightRec(node.left)
                 hr = heightRec(node.right)
                 return 1+max(hl,hr)
-             return 0
-         return heightRec(self.root)-1
+            return 0
+        return heightRec(self.root)-1
     
     def leafcounter(self):
         def recursiveleafcounter(node):
 
-            if node is not None:
+            if node:
                 if node.left==None and node.right==None: #leaf node
                     return 1
                 return recursiveleafcounter(node.left) + recursiveleafcounter(node.right)
             return 0 #root is None
         
         return recursiveleafcounter(self.root)
+    
+    def delete(self,data):
+        def delete_recursive(node,data):
+            if node is None: #raiz é none
+                return None
+            
+            #Percorre a arvore
+            if data<node.data:
+                node.left = delete_recursive(node.left,data)
+            elif data>node.data:
+                node.right = delete_recursive(node.right,data)
+            
+            #Vc ta retornando pro valor que quer remover
+            #Ex:10r,nonee,20d. 20,15e,noned. Quer remover 20. enquanto ta no 10, fala q o 20 é igual delrecurs 20.Cai no caso que esq existe,dir none, retorna esq no lugar do 20,assim remove o 20
+            else:
+                if not node.left and not node.right: #nao tem filhos, dir e esq sao none
+                    return None
+                elif not node.left and node.right: #esq none, dir existe
+                    return node.right
+                elif not node.right and node.left: #dir none, esq existe
+                    return node.left
+                
+                else: #tem 2 filhos
+                    cur=node.right
+                    while cur.left is not None: #Minimo subarvore direita
+                        cur=cur.left
+                    
+                    node.data=cur.data #copia minimo subarvore dir pra o node a ser removido
+                    node.right=delete_recursive(node.right,cur.data) #deleta recursivamente o min subarvore dir
+
+            return node     
+        self.root= delete_recursive(self.root,data)
+    
+    def search(self,data):
+        def search_recursive(node,data):
+            if node is None:
+                return False
+            
+            elif data==node.data: #Caso base, achou
+                return True
+            
+            elif data<node.data:
+                return search_recursive(node.left,data)
+            
+            elif data>node.data:
+                return search_recursive(node.right,data)  
+
+        return search_recursive(self.root,data)
 
 
 t= BinarySearchTree(10)
@@ -70,6 +115,12 @@ t.insert(20)
 t.insert(15)
 t.insert(8)
 t.print()
+
+t.delete(5)
+t.print()
+
+print(t.search(15))
+print(t.search(16))
 
 print("height:", t.height())
 print("leaves:", t.leafcounter())
